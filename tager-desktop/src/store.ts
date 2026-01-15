17f86f1b7f504b3bd4f562b46a837acab959a155
Paweł Hase <pawelhase@gmail.com>
2026-01-15 22:24:06 +0100

    removed old store

--- tager-desktop/src/store.ts
+++ tager-desktop/src/store.ts
@@ -1,28 +1,6 @@
 import { create } from "zustand";
-import type { FileItem } from "./types";
 import * as api from "./api/tager";
 import type { FileEntry, ManagerStatus, TagEntry } from "./types/tager";
-
-type FileStore = {
-  selectedIndex: number | null,
-  files: FileItem[],
-  setFiles: (filesInfos: FileItem[]) => void;
-  select: (index: number | null) => void;
-  updateFile: (index: number | null, data: Partial<FileItem>) => void;
-}
-
-export const useFileStore = create<FileStore>((set) => ({
-  selectedIndex: null,
-  files: [],
-  setFiles: (files) => set({ files }),
-  select: (index) => set({ selectedIndex: index }),
-  updateFile: (index, data) =>
-    set((state) => ({
-      files: state.files.map((n, i) =>
-        i === index ? { ...n, ...data } : n
-      ),
-    }))
-}))
 
 const ensureSelectionValid = (
   files: FileEntry[],
